70578ac7e9b1c51c726d8618bd0a1cec79bda0cf
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-05-31 04:15:02 +0600

    initial env setup and llm invoked

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.prompts import PromptTemplate
+
+llm = Ollama(model="llama3")
+response = llm.invoke("tell a cat joke")
+print(response)
